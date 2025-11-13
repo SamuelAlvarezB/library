311997b14dc94978811a00acf88f7f55a2a6fc1a
Samuel Alvarez <abrena2@gmail.com>
2025-11-13 20:04:58 +0100

    Add basic HTML, CSS and JavaScript sctructure

---
+++ script.js
@@ -0,0 +1,6 @@
+const library = document.getElementById("library");
+
+/*let book = document.createElement("div");
+book.classList.add("book-card");
+book.innerHTML = "<p>Title: The Hobbit</p><p>Author: J.R.R. Tolkien</p><p>Number of pages: 296</p><p>Description: Lorem ipsum dolor aemet.</p>";
+library.appendChild(book);*/
